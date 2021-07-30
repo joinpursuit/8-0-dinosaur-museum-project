de8756433fc9be49c8caa3df84273efab481c89b
Christina Loiacono <loiaconochristina@gmail.com>
2021-07-30 16:32:27 -0400

    added code for file 3

--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -54,7 +54,43 @@ const exampleTicketData = require("../data/tickets");
     calculateTicketPrice(tickets, ticketInfo);
     //> "Entrant type 'kid' cannot be found."
  */
-function calculateTicketPrice(ticketData, ticketInfo) {}
+function calculateTicketPrice(ticketData, ticketInfo) {
+  let totalPrice = 0; // track it
+  if (ticketInfo.ticketType in ticketData) {
+      // check if it's there, if false - error message. if entrantType
+      if (ticketInfo.entrantType in ticketData[ticketInfo.ticketType].priceInCents) {
+        totalPrice += ticketData[ticketInfo.ticketType].priceInCents[ticketInfo.entrantType]; // to access the price in the object
+      }
+      else return `Entrant type '${ticketInfo.entrantType}' cannot be found.`;
+    }
+    else return `Ticket type 'incorrect-type' cannot be found.`;
+    if (ticketInfo.extras.length) {
+      // check if its inside the extras object - loop through
+      let extrasArr = ticketInfo.extras.slice(0); // makes a copy
+
+      for (let elem of extrasArr) {
+        // need to get only the extras object, and then check if our key is one of the keys - not make it static. there's 3 different ones.
+        if (elem in ticketData.extras) { // check if its a valid key - enable access
+          totalPrice += ticketData.extras[elem].priceInCents[ticketInfo.entrantType];
+        }
+        else return `Extra type '${elem}' cannot be found.`;
+      }
+  }
+  return totalPrice;
+// if extras exist, this happens:
+// for (let ticket of ticketData) {}
+// check if its an empty array
+
+}
+
+// if (ticketInfo.ticketType || ticketInfo.entrantType)
+  // If either the `ticketInfo.ticketType` value or `ticketInfo.entrantType` value is incorrect, or any of the values inside of the `ticketInfo.extras` key is incorrect, an error message should be returned.
+  // let errorMsg = "Error.";
+  // errorMsg;
+
+  // Returns the ticket price based on the ticket information supplied to the function
+// return totalPrice // number - The cost of the ticket in cents.
+
 
 /**
  * purchaseTickets()
@@ -109,7 +145,105 @@ function calculateTicketPrice(ticketData, ticketInfo) {}
     purchaseTickets(tickets, purchases);
     //> "Ticket type 'discount' cannot be found."
  */
-function purchaseTickets(ticketData, purchases) {}
+
+
+/**
+ * helper function
+ * @param {*} ticketData - object of tickets
+ * @param {*} ticketInfo - info about a ticket
+ * @returns {string} - returns a formatted string
+ * @description - if empty, should not return anything
+// if has an input/multiple inputs, should return those inputs
+
+ */
+// for each element to access. check if its the last
+      // newStr += ticketData.extras[elem].description
+
+function formatted(ticketData, ticketInfo) {
+  let newArr = ticketInfo.extras.slice(0); // makes a copy of extras
+  let newStr = "";
+  if (newArr.length) { // if more than zero
+    newStr += " ("; // if we are sure its just 1
+    for (let i = 0; i < newArr.length; i++) { 
+      if (i === newArr.length -1) { // if i equals last index element
+        newStr += ticketData.extras[newArr[i]].description + ")";
+      } // if more than 1 index element
+      else newStr += ticketData.extras[newArr[i]].description + ", ";
+    }
+  }
+  return newStr; // if 0 in extras
+}
+// need to check if this helper function works or not
+// console.log(formatted(ticketData, ticketInfo));
+
+
+/**
+ * another helper function
+ * @param {string} - str
+ * @returns {string} - returns a string with the first letter in cap letter.
+ */
+ function capitalize(str){
+  let first  = str[0].toUpperCase();
+  let newArr = str.split("");
+  newArr.shift();
+  return first + newArr.join("");
+}
+
+  // call the function from before with parameters
+  // check if it is a number
+  // use helper function to get multiple inputs for extras/ the string with parentheses, and call it here
+  // ticket.ticketType - to choose btwn membership or general, be dynamic
+
+function purchaseTickets(ticketData, purchases) {
+  let newStr = "Thank you for visiting the Dinosaur Museum!\n-------------------------------------------\n";
+  let total = 0; // keep track
+  for (let ticket of purchases) { // for loop - Adult, Senior, Child
+    let subTotal = 0;
+    let check = calculateTicketPrice(ticketData, ticket);
+    
+    // you could check if it's not a number first, to see if theres an error & return the error right away
+    if (typeof check === "number") {
+      subTotal = check;
+      newStr += `${capitalize(ticket.entrantType)} ${ticketData[ticket.ticketType].description}: $${(subTotal/100).toFixed(2)}${formatted(ticketData, ticket)}\n`;
+      total += subTotal/100; // add subtotal to the price
+    }
+    else return check;
+  }
+  return newStr += "-------------------------------------------\nTOTAL: $" +total.toFixed(2); // outside for loop
+}
+
+// function capitalize(str) {
+//   let first = str[0].toUpperCase()
+//   let newStr = str.slice(1);
+//   return first + newStr;
+// }
+// console.log(capitalize("christina"));
+
+// function capitalize2(str) {
+//   let first = str[0].toUpperCase()
+//   let newStr = str.split("");
+//   newStr.shift();
+//   return first + newStr.join("");
+// }
+// console.log(capitalize2("christina2"));
+
+
+
+// ${ticket.ticketType}
+
+
+
+  // if statement - for if extras
+  // need to access the values for inputs:
+  // (general + (extras: movie + terrace));
+
+// calculate price in dollars for each
+// Adult, Senior, Child
+
+// then access the values/price for extras, and add those on.
+
+// return `Thank you for visiting the Dinosaur Museum!\n-------------------------------------------\nAdult General Admission: $50.00 (Movie Access, Terrace Access)\nSenior General Admission: $35.00 (Terrace Access)\nChild General Admission: $45.00 (Education Access, Movie Access, Terrace Access)\nChild General Admission: $45.00 (Education Access, Movie Access, Terrace Access)\n-------------------------------------------\nTOTAL: $175.00`;
+// }
 
 // Do not change anything below this line.
 module.exports = {
