17c17e078a431bb4f3feb43a4a1e4c10103ce8d7
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2021-08-04 09:54:49 -0400

    almost finished

--- src/02-room-details.js
+++ src/02-room-details.js
@@ -1,7 +1,11 @@
 /*
-  Do not change the lines below. If you'd like to run code from this file, you may use the `exampleDinosaurData` and `exampleRoomData` variables below to gain access to each data set. This data is pulled from the relevant files in the `data/` directory.
+  Do not change the lines below. If you'd like to run code from this file, 
+  you may use the `exampleDinosaurData` and `exampleRoomData` variables 
+  below to gain access to each data set. This data is pulled from the 
+  relevant files in the `data/` directory.
 
-  You may use this data to test your functions. You may assume the shape of the data remains the same but that the values may change.
+  You may use this data to test your functions. You may assume the shape 
+  of the data remains the same but that the values may change.
 */
 const exampleDinosaurData = require("../data/dinosaurs");
 const exampleRoomData = require("../data/rooms");
@@ -10,12 +14,17 @@ const exampleRoomData = require("../data/rooms");
 /**
  * getRoomByDinosaurName()
  * ---------------------
- * Return the name of the room where the given dinosaur can be found. If the dinosaur does not exist in the `dinosaurs` list or cannot be found in any room, return an error message that says so.
+ * Return the name of the room where the given dinosaur can be found. 
+ * If the dinosaur does not exist in the `dinosaurs` list or cannot be 
+ * found in any room, return an error message that says so.
  *
- * @param {Object[]} dinosaurs - An array of dinosaur objects. See the `data/dinosaurs.js` file for an example of the input.
- * @param {Object[]} rooms - An array of room objects. See the `data/rooms.js` file for an example of the input.
+ * @param {Object[]} dinosaurs - An array of dinosaur objects. See the 
+ * `data/dinosaurs.js` file for an example of the input.
+ * @param {Object[]} rooms - An array of room objects. See the `data/rooms.js` 
+ * file for an example of the input.
  * @param {string} dinosaurName - The name of the dinosaur.
- * @returns {string} The name of the room where the dinosaur can be found. Alternatively, an error message.
+ * @returns {string} The name of the room where the dinosaur can be found. 
+ * Alternatively, an error message.
  *
  * EXAMPLE:
  *  getRoomByDinosaurName(dinosaurs, rooms, "Tyrannosaurus");
@@ -25,14 +34,86 @@ const exampleRoomData = require("../data/rooms");
  *  getRoomByDinosaurName(dinosaurs, rooms, "Pterodactyl");
  *  //> "Dinosaur with name 'Pterodactyl' cannot be found."
  */
-function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {}
+
+  // Input: Dinosaurs[{}], rooms[{}], dinosaurName(string)
+  // Output: Name (string) of the room where dinosaourName can be found
+
+  // Approach: loop through DINOSAURS[{}]
+  // if dinosaurName matches NAME in DINOSAURS[{}]
+  // use the dinosaurID, loop through both arrays
+  // to find the match through dinosaurId in DINOSAURS[{}] and through dinosaurs[] in ROOMS[{}]
+  // If matched, return the room NAME from rooms
+
+  // edge cases: string - loop through DINOSAURS[{}] 
+  // if dinosaurName doesn't match .name
+  // return "Dinosaur with name `dinosaurName' cannot be found."
+
+function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {
+
+  // declare 'foundDinoId' and assign an empty string
+  let foundDinoId = '';
+  // declare 'foundDinoRoom' and assign an empty string
+  let foundDinoRoom = '';
+
+  // iterate through each 'dinosaur' in 'dinosaurs'
+  for (const dinosaur of dinosaurs) {
+    // if dinosaurName matches 'dinosaur.name' 
+    if (dinosaurName === dinosaur.name) {
+      // reassign 'foundDinoId' to 'dinosaur.dinosaurId'
+      foundDinoId = dinosaur.dinosaurId;
+    } 
+  }
+  // edge case #1
+  if (!foundDinoId) {
+    foundDinoRoom = `Dinosaur with name '${dinosaurName}' cannot be found.`
+  }
+  
+  // iterate through each 'room' in 'rooms'
+  for (const room of rooms) {
+    // if 'room.dinosaurs' includes 'foundDinoId'
+    if (room.dinosaurs.includes(foundDinoId)) {
+      // reassign 'foundDinoRoom' to 'room.name'
+      foundDinoRoom = room.name
+    }
+  }
+  // edge case #2
+  if (!foundDinoRoom) {
+    foundDinoRoom = `Dinosaur with name '${dinosaurName}' cannot be found in any rooms.`
+  }
+  
+
+  // return modified 'foundDinoRoom';
+  return foundDinoRoom;
+
+
+
+
+
+  // for (const dinosaur of dinosaurs) {
+  //   if (dinosaurName !== dinosaur.name) {
+  //     return `Dinosaur with name '${dinosaurName}' cannot be found.`;
+  //   }
+  // }
+
+  // for (const room of rooms) {
+  //   for (const dinoId of room.dinosaurs) {
+  //     if (dinosaurName !== dinoId) {
+  //       return `Dinosaur with name '${dinosaurName}' cannot be found in any rooms.`;
+  //     }
+  //   }
+  // }
+}
+
+console.log(getRoomByDinosaurName(exampleDinosaurData, exampleRoomData, "Tyrannosaurus"));
 
 /**
  * getConnectedRoomNamesById()
  * ---------------------
- * Returns an array of strings, where each string is the name of a room connected to the given room. If a room ID cannot be found, an error message is returned.
+ * Returns an array of strings, where each string is the name of a room connected  
+ * to the given room. If a room ID cannot be found, an error message is returned.
  *
- * @param {Object[]} rooms - An array of room objects. See the `data/rooms.js` file for an example of the input.
+ * @param {Object[]} rooms - An array of room objects. See the `data/rooms.js` 
+ * file for an example of the input.
  * @param {string} id - A unique room identifier.
  * @returns {string|string[]} An array of room names, or an error message.
  *
@@ -49,7 +130,60 @@ function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {}
       "Kit Hopkins Education Wing"
     ]
  */
-function getConnectedRoomNamesById(rooms, id) {}
+function getConnectedRoomNamesById(rooms, id) {
+  // // declare 'arrOfRooms' and assign an empty array
+  // const arrOfRooms = [];
+  // // declare 'connectsId' and assign an empty array
+  // let connectsId = [];
+
+  // // iterate through each 'room' in 'rooms'
+  // for (const room of rooms) {
+  //   // if 'room.roomId' matches 'id'
+  //   if (room.roomId === id) {
+  //     // 'connectsId' is reassigned to 'room.connectsTo'
+  //     connectsId = room.connectsTo;
+  //   }
+  // }
+
+  // // edge case #1
+  // if (connectsId.length === 0) {
+  //   return `Room with ID of '${id}' could not be found.`;
+  // }
+  
+  // // iterate through each 'room' in 'rooms'
+  // for (const room of rooms) {
+  //   // if 'connectsId' includes 'room.roomId'
+  //   if (connectsId.includes(room.roomId)) {
+  //     // push 'room.name' into 'arrOfRooms'
+  //     arrOfRooms.push(room.name);
+  //   }
+  // }
+  // // return modified 'arrOfRooms'
+  // return arrOfRooms;
+
+
+  let connectedNames = [];
+
+  for (const room of rooms) {
+    for (const connect of room.connectsTo) {
+      if (id === connect && connect !== 'incorrect-id') {
+        connectedNames.push(room.name);
+      } else if (connect === 'incorrect-id') {
+        return `Room with ID of 'incorrect-id' could not be found.`;
+      }
+    }
+  }
+
+  if (connectedNames.length === 0) return `Room with ID of '${id}' could not be found.`;
+
+  return connectedNames;
+
+
+
+  
+}
+
+getConnectedRoomNamesById(exampleRoomData, "A6QaYdyKra");
 
 module.exports = {
   getRoomByDinosaurName,
--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -1,9 +1,13 @@
 /*
-  Do not change the line below. If you'd like to run code from this file, you may use the `exampleTicketData` variable below to gain access to tickets data. This data is pulled from the `data/tickets.js` file.
+  Do not change the line below. If you'd like to run code from this file, 
+  you may use the `exampleTicketData` variable below to gain access to tickets data. 
+  This data is pulled from the `data/tickets.js` file.
 
-  You may use this data to test your functions. You may assume the shape of the data remains the same but that the values may change.
+  You may use this data to test your functions. You may assume the shape of the 
+  data remains the same but that the values may change.
 
-  Keep in mind that your functions must still have and use a parameter for accepting all tickets.
+  Keep in mind that your functions must still have and use a parameter 
+  for accepting all tickets.
 */
 const exampleTicketData = require("../data/tickets");
 // Do not change the line above.
@@ -11,20 +15,28 @@ const exampleTicketData = require("../data/tickets");
 /**
  * calculateTicketPrice()
  * ---------------------
- * Returns the ticket price based on the ticket information supplied to the function. The `ticketInfo` will be in the following shape. See below for more details on each key.
+ * Returns the ticket price based on the ticket information supplied to the function. 
+ * The `ticketInfo` will be in the following shape. See below for more details on each key.
  * const ticketInfo = {
     ticketType: "general",
     entrantType: "child",
     extras: ["movie"],
   };
  *
- * If either the `ticketInfo.ticketType` value or `ticketInfo.entrantType` value is incorrect, or any of the values inside of the `ticketInfo.extras` key is incorrect, an error message should be returned.
+ * If either the `ticketInfo.ticketType` value or `ticketInfo.entrantType` 
+ * value is incorrect, or any of the values inside of the `ticketInfo.extras` 
+ * key is incorrect, an error message should be returned.
  *
- * @param {Object} ticketData - An object containing data about prices to enter the museum. See the `data/tickets.js` file for an example of the input.
+ * @param {Object} ticketData - An object containing data about prices to enter the museum. 
+ * See the `data/tickets.js` file for an example of the input.
  * @param {Object} ticketInfo - An object representing data for a single ticket.
- * @param {string} ticketInfo.ticketType - Represents the type of ticket. Could be any string except the value "extras".
- * @param {string} ticketInfo.entrantType - Represents the type of entrant. Prices change depending on the entrant.
- * @param {string[]} ticketInfo.extras - An array of strings where each string represent a different "extra" that can be added to the ticket. All strings should be keys under the `extras` key in `ticketData`.
+ * @param {string} ticketInfo.ticketType - Represents the type of ticket. 
+ * Could be any string except the value "extras".
+ * @param {string} ticketInfo.entrantType - Represents the type of entrant. 
+ * Prices change depending on the entrant.
+ * @param {string[]} ticketInfo.extras - An array of strings where each string 
+ * represent a different "extra" that can be added to the ticket. All strings 
+ * should be keys under the `extras` key in `ticketData`.
  * @returns {number} The cost of the ticket in cents.
  *
  * EXAMPLE:
@@ -54,22 +66,72 @@ const exampleTicketData = require("../data/tickets");
     calculateTicketPrice(tickets, ticketInfo);
     //> "Entrant type 'kid' cannot be found."
  */
-function calculateTicketPrice(ticketData, ticketInfo) {}
+
+function calculateTicketPrice(ticketData, ticketInfo) {
+  // declare 'total' and assign 0
+  let total = 0;
+
+  // if (in) === .includes for objects 
+  // note to self: don't have to loop through objects, compared to arrays
+  // bc of how objects can point to their values through dot/bracket notation
+  
+  // Errors
+  // If ticketData doesnt evaluate to 'general' or 'membership'
+  if (!ticketData[ticketInfo.ticketType]) {
+    // return an error message
+    return `Ticket type '${ticketInfo.ticketType}' cannot be found.`;
+  }
+  
+  // If ticketData's age description doesnt evaluate to 'child', 'adult', or 'senior'
+  if (!ticketData[ticketInfo.ticketType].priceInCents[ticketInfo.entrantType]) {
+    // return an error message
+    return `Entrant type '${ticketInfo.entrantType}' cannot be found.`;
+  }
+  // iterate throuch each 'extra' in 'extras' array
+  for (const extra of ticketInfo.extras) {
+    // If 'extra' isn't in ticketData's extras
+    if (!(extra in ticketData.extras)) {
+      // return an error message
+      return `Extra type 'incorrect-extra' cannot be found.`;
+    }
+  }
+
+  // 'total' is reassigned the 'priceInCents' value based on ticket type and entrant type
+  total = ticketData[ticketInfo.ticketType].priceInCents[ticketInfo.entrantType];
+
+  // part 2, for a ticket with 'extras', extra costs added
+  // iterate through each 'extra' in 'ticketInfo.extras'
+  for (const extra of ticketInfo.extras) {
+    // reassign total by adding 'extra' costs to it
+    total += ticketData.extras[extra].priceInCents[ticketInfo.entrantType];
+  }
+  // return modifed total
+  return total;
+}
 
 /**
  * purchaseTickets()
  * ---------------------
- * Returns a receipt based off of a number of purchase. Each "purchase" maintains the shape from `ticketInfo` in the previous function.
+ * Returns a receipt based of a number of purchases. 
+ * Each "purchase" maintains the shape from `ticketInfo` in the previous function.
  *
- * Any errors that would occur as a result of incorrect ticket information should be surfaced in the same way it is in the previous function.
+ * Any errors that would occur as a result of incorrect ticket information 
+ * should be surfaced in the same way it is in the previous function.
  * 
- * NOTE: Pay close attention to the format in the examples below and tests. You will need to have the same format to get the tests to pass.
+ * NOTE: Pay close attention to the format in the examples below and tests. 
+ * You will need to have the same format to get the tests to pass.
  *
- * @param {Object} ticketData - An object containing data about prices to enter the museum. See the `data/tickets.js` file for an example of the input.
- * @param {Object[]} purchases - An array of objects. Each object represents a single ticket being purchased.
- * @param {string} purchases[].ticketType - Represents the type of ticket. Could be any string except the value "extras".
- * @param {string} purchases[].entrantType - Represents the type of entrant. Prices change depending on the entrant.
- * @param {string[]} purchases[].extras - An array of strings where each string represent a different "extra" that can be added to the ticket. All strings should be keys under the `extras` key in `ticketData`.
+ * @param {Object} ticketData - An object containing data about prices to enter 
+ * the museum. See the `data/tickets.js` file for an example of the input.
+ * @param {Object[]} purchases - An array of objects. Each object represents a 
+ * single ticket being purchased.
+ * @param {string} purchases[].ticketType - Represents the type of ticket. 
+ * Could be any string except the value "extras".
+ * @param {string} purchases[].entrantType - Represents the type of entrant. 
+ * Prices change depending on the entrant.
+ * @param {string[]} purchases[].extras - An array of strings where each string 
+ * represent a different "extra" that can be added to the ticket. All strings 
+ * should be keys under the `extras` key in `ticketData`.
  * @returns {string} A full receipt, with each individual ticket bought and the total.
  *
  * EXAMPLE:
@@ -96,7 +158,13 @@ function calculateTicketPrice(ticketData, ticketInfo) {}
       },
     ];
     purchaseTickets(tickets, purchases);
-    //> "Thank you for visiting the Dinosaur Museum!\n-------------------------------------------\nAdult General Admission: $50.00 (Movie Access, Terrace Access)\nSenior General Admission: $35.00 (Terrace Access)\nChild General Admission: $45.00 (Education Access, Movie Access, Terrace Access)\nChild General Admission: $45.00 (Education Access, Movie Access, Terrace Access)\n-------------------------------------------\nTOTAL: $175.00"
+
+    //> "Thank you for visiting the Dinosaur Museum!\n------------------
+    -------------------------\nAdult General Admission: $50.00 (Movie Access, 
+    Terrace Access)\nSenior General Admission: $35.00 (Terrace Access)\nChild 
+    General Admission: $45.00 (Education Access, Movie Access, Terrace Access)
+    \nChild General Admission: $45.00 (Education Access, Movie Access, Terrace 
+    Access)\n-------------------------------------------\nTOTAL: $175.00"
 
  * EXAMPLE:
     const purchases = [
@@ -109,7 +177,98 @@ function calculateTicketPrice(ticketData, ticketInfo) {}
     purchaseTickets(tickets, purchases);
     //> "Ticket type 'discount' cannot be found."
  */
-function purchaseTickets(ticketData, purchases) {}
+
+// Helper function to semi-format the price
+function toDollar(result) {
+  return result / 100;
+}
+
+// Helper function to uppercase the first letter of entrant type 'adult' --> 'Adult'
+function uppercase(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function purchaseTickets(ticketData, purchases) {
+  // declare 'eachTicketPrice' and assign an empty string
+  let eachTicketPrice = true;
+  // declare 'totalPrice' and assign 0
+  let totalPrice = 0;
+  // declare 'receipt' and assign an empty string
+  let receipt = '';
+
+  // iterate throuch each 'ticketInfo' in 'purchases'
+  for (const ticketInfo of purchases) {
+    // reassign 'ticketInfo' to the evaluated result of calling function
+    // 'calculateTicketPrice' and passing 'ticketData', 'ticketInfo' as arguments
+    eachTicketPrice = calculateTicketPrice(ticketData, ticketInfo);
+      // if the type of 'eachTicketPrice' is a string (means it's the error messages)
+      if (typeof eachTicketPrice === 'string') {
+        // return 'eachTicketPrice'
+        return eachTicketPrice;
+      } 
+    // in the case 'eachTicketPrice' is not a string, reassign & add it 'totalPrice' each iteration
+    totalPrice += eachTicketPrice;
+
+    // reassign receipt to each relative 'ticketInfo', with 'eachPriceTicket' and 'totalPrice'
+    receipt = `Thank you for visiting the Dinosaur Museum!\n-------------------------------------------\n${uppercase(ticketInfo.entrantType)} ${ticketData[ticketInfo.ticketType].description}: $${toDollar(eachTicketPrice).toFixed(2)}\n-------------------------------------------\nTOTAL: $${toDollar(totalPrice).toFixed(2)}`;
+  }
+
+
+
+
+
+
+
+
+
+  
+  // // iterate through 'eachPurchase' in 'purchases'
+  // for (const eachPurchase of purchases) {
+  //   //
+  //   const admissionType = ticketData[eachPurchase.ticketType];
+  //   //
+  //   const ageType = ticketData[eachPurchase.ticketType].priceInCents[eachPurchase.entrantType];
+  //   // if 'ticketData[eachPurchase.ticketType]' is truthy
+  //   if (admissionType) {
+  //     // if 'ticketData[eachPurchase.ticketType].priceInCents[eachPurchase.entrantType]' is truthy
+  //     if (ageType) {
+  //       // iterate through 'eachExtra' in 'eachPurchase.extras'
+  //       for (const eachExtra of eachPurchase.extras) {
+  //         // if 'ticketData[eachExtra]' is truthy
+  //         if (ticketData[eachExtra]) {
+  //           // reassign 'eachTicketPrice' to calling function 'calculateTicketPrice'
+  //           // and passing 'ticketData', 'eachPurchase' as arguments
+  //           eachTicketPrice = calculateTicketPrice(ticketData, eachPurchase);
+  //           // reassign and add 'eachTicketPrice' to 'totalPrice', each function invocation
+  //           totalPrice += eachTicketPrice;
+  //           // reassign and format 'totalPrice' by invoking helper function 'toDollar'
+  //           totalPrice = toDollar(totalPrice);
+
+  //           // reassign 'receipt' to `Thank you for visiting the Dinosaur Museum!\n------------------
+  //           // -------------------------\n${entrantType} ${ticketData[ticketType].description}: 
+  //           // ${eachTicketPrice}\n-------------------------------------------\nTOTAL: ${totalPrice}`
+  //           receipt = `Thank you for visiting the Dinosaur Museum!\n------------------
+  //           -------------------------\n${ageType} ${admissionType.description}: 
+  //           ${eachTicketPrice}\n-------------------------------------------\nTOTAL: ${totalPrice}`;
+  //         }
+  //       }
+  //     }
+  //   }    
+  // }
+
+  // return modified 'receipt'
+  return receipt;
+}
+
+const purchases = [
+  {
+    ticketType: "general",
+    entrantType: "adult",
+    extras: ["movie", "terrace"],
+  }
+];
+
+purchaseTickets(exampleTicketData, purchases);
 
 // Do not change anything below this line.
 module.exports = {
