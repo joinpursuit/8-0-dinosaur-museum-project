eeebbf7b12746d4c0f6f9d3310d85c3f00da71d2
Melissa Lopez <1zuzsuit492@Melissas-MacBook-Pro.local>
2021-08-18 22:50:28 -0400

    completed lab

--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -103,8 +103,6 @@ for(let i = 0; i < ticketInfo.extras.length; i++){
   }
   total += ticketData.extras[ticketInfo.extras[i]].priceInCents[ticketInfo.entrantType];
 
-
-
 }
 return total;
 }
@@ -112,16 +110,23 @@ return total;
 /**
  * purchaseTickets()
  * ---------------------
- * Returns a receipt based off of a number of purchase. Each "purchase" maintains the shape from `ticketInfo` in the previous function.
+ * Returns a receipt based off of a number of purchase. 
+ * Each "purchase" maintains the shape from `ticketInfo` in the previous function.
  *
- * Any errors that would occur as a result of incorrect ticket information should be surfaced in the same way it is in the previous function.
+ * Any errors that would occur as a result of incorrect ticket information, 
+ * should be surfaced in the same way it is in the previous function.
  * 
- * NOTE: Pay close attention to the format in the examples below and tests. You will need to have the same format to get the tests to pass.
+ * NOTE: Pay close attention to the format in the examples below and tests. 
+ * You will need to have the same format to get the tests to pass.
  *
- * @param {Object} ticketData - An object containing data about prices to enter the museum. See the `data/tickets.js` file for an example of the input.
- * @param {Object[]} purchases - An array of objects. Each object represents a single ticket being purchased.
- * @param {string} purchases[].ticketType - Represents the type of ticket. Could be any string except the value "extras".
- * @param {string} purchases[].entrantType - Represents the type of entrant. Prices change depending on the entrant.
+ * @param {Object} ticketData - An object containing data about prices to enter the museum. 
+ * See the `data/tickets.js` file for an example of the input.
+ * @param {Object[]} purchases - An array of objects. 
+ * Each object represents a single ticket being purchased.
+ * @param {string} purchases[].ticketType - Represents the type of ticket. 
+ * Could be any string except the value "extras".
+ * @param {string} purchases[].entrantType - Represents the type of entrant. 
+ * Prices change depending on the entrant.
  * @param {string[]} purchases[].extras - An array of strings where each string represent a different "extra" that can be added to the ticket. All strings should be keys under the `extras` key in `ticketData`.
  * @returns {string} A full receipt, with each individual ticket bought and the total.
  *
@@ -162,9 +167,33 @@ return total;
     purchaseTickets(tickets, purchases);
     //> "Ticket type 'discount' cannot be found."
  */
-function purchaseTickets(ticketData, purchases) {
-  //loop through purchases 
-}
+
+    function purchaseTickets(ticketData, purchases) {
+      let total = 0; 
+      let receipt = 'Thank you for visiting the Dinosaur Museum!\n-------------------------------------------\n'
+      for(let purchase of purchases){ //purchases[i] loop
+        let entrantType = purchase.entrantType;
+        let ticketType = purchase.ticketType;
+        let ticketPriceInCents = calculateTicketPrice(ticketData, purchase); //amount from previous function
+        let ticketPriceInDollars = ticketPriceInCents / 100; //convert to dollars by dividing by 100
+        total += ticketPriceInDollars;
+      
+        if(typeof(calculateTicketPrice(ticketData, purchase)) === 'string'){
+          return calculateTicketPrice(ticketData, purchase);
+        };
+        for(let i = 0; i < purchase.extras.length; i++){ // looping through extras
+          purchase.extras[i] = purchase.extras[i].charAt(0).toUpperCase() + purchase.extras[i].slice(1) + ' Access';
+        };
+         if(purchase.extras.length){
+          receipt += entrantType.charAt(0).toUpperCase() + entrantType.slice(1) + ' ' + ticketType.charAt(0).toUpperCase() + ticketType.slice(1) + ' Admission: $' + ticketPriceInDollars.toFixed(2) + ' (' + purchase.extras.join(', ') + `)\n`;
+        }else{
+          receipt += entrantType.charAt(0).toUpperCase() + entrantType.slice(1) + ' ' + ticketType.charAt(0).toUpperCase() + ticketType.slice(1) + ' Admission: $' + ticketPriceInDollars.toFixed(2) + `\n`;
+        };
+      };
+      return receipt + `-------------------------------------------\nTOTAL: $${total.toFixed(2)}`;
+    }
+
+
 
 // Do not change anything below this line.
 module.exports = {
