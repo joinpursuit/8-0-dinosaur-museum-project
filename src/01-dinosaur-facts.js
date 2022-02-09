0844c0b826fa87c683604e41dae07868049fc2f4
Zane <zaneahmed4@gmail.com>
2022-02-08 21:24:02 -0500

    01 done

--- src/01-dinosaur-facts.js
+++ src/01-dinosaur-facts.js
@@ -22,7 +22,20 @@ const exampleDinosaurData = require("../data/dinosaurs");
  *  getTallestDinosaur(dinosaurs);
  *  //> { Brachiosaurus: 98.43 }
  */
-function getTallestDinosaur(dinosaurs) {}
+function getTallestDinosaur(dinosaurs) {
+  let obj = {}
+  let tall = dinosaurs[0]
+
+  for(let i = 1; i < dinosaurs.length; i++){
+    if(dinosaurs[i].lengthInMeters > tall.lengthInMeters){
+      tall = dinosaurs[i]    
+  }
+  }
+    if(tall){
+      obj[tall.name] = tall.lengthInMeters * 3.281
+  }
+  return obj
+}
 
 /**
  * getDinosaurDescription()
@@ -44,7 +57,14 @@ function getTallestDinosaur(dinosaurs) {}
  *  getDinosaurDescription(dinosaurs, "incorrect-id");
  *  //> "A dinosaur with an ID of 'incorrect-id' cannot be found."
  */
-function getDinosaurDescription(dinosaurs, id) {}
+function getDinosaurDescription(dinosaurs, id) {
+  for(let i = 0; i < dinosaurs.length; i++){
+    if(dinosaurs[i].dinosaurId === id){
+      return `${dinosaurs[i].name} (${dinosaurs[i].pronunciation})\n${dinosaurs[i].info} It lived in the ${dinosaurs[i].period} period, over ${dinosaurs[i].mya[dinosaurs[i].mya.length-1]} million years ago.`
+    }  
+}
+  return `A dinosaur with an ID of 'incorrect-id' cannot be found.`
+}
 
 /**
  * getDinosaursAliveMya()
@@ -71,7 +91,31 @@ function getDinosaurDescription(dinosaurs, id) {}
  *  getDinosaursAliveMya(dinosaurs, 65, "unknown-key");
  *  //> ["WHQcpcOj0G"]
  */
-function getDinosaursAliveMya(dinosaurs, mya, key) {}
+function getDinosaursAliveMya(dinosaurs, mya, key) {
+  let alive = []
+  
+  for(let i = 0; i < dinosaurs.length; i++){
+
+  if(dinosaurs[i].mya.length === 1){
+    if(mya === dinosaurs[i].mya[0] || mya === dinosaurs[i].mya[0] -1 ){
+    if(key in dinosaurs[i]){
+        alive.push(dinosaurs[i][key])
+      } else {
+        alive.push(dinosaurs[i].dinosaurId)
+      }
+  } 
+  } else {
+     if(mya >= dinosaurs[i].mya[1] && mya <= dinosaurs[i].mya[0] ){
+      if(key in dinosaurs[i]){
+          alive.push(dinosaurs[i][key])
+        } else {
+          alive.push(dinosaurs[i].dinosaurId)
+        }
+    } 
+  }
+  }
+  return alive
+}
 
 module.exports = {
   getTallestDinosaur,
