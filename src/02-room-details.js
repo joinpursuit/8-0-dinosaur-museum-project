e70a820fb7d819eac76d3c6c0ba565bf9658deaa
Derek <plutus.derekpeterson@gmail.com>
2022-02-09 23:16:12 -0500

    Completed 02-room-details.js

--- src/02-room-details.js
+++ src/02-room-details.js
@@ -1,5 +1,6 @@
 /*
-  Do not change the lines below. If you'd like to run code from this file, you may use the `exampleDinosaurData` and `exampleRoomData` variables below to gain access to each data set. This data is pulled from the relevant files in the `data/` directory.
+  Do not change the lines below. If you'd like to run code from this file, you may use the `exampleDinosaurData` and `exampleRoomData` 
+  variables below to gain access to each data set. This data is pulled from the relevant files in the `data/` directory.
 
   You may use this data to test your functions. You may assume the shape of the data remains the same but that the values may change.
 */
@@ -10,7 +11,8 @@ const exampleRoomData = require("../data/rooms");
 /**
  * getRoomByDinosaurName()
  * ---------------------
- * Return the name of the room where the given dinosaur can be found. If the dinosaur does not exist in the `dinosaurs` list or cannot be found in any room, return an error message that says so.
+ * Return the name of the room where the given dinosaur can be found. If the dinosaur does not exist in the `dinosaurs` list
+ * or cannot be found in any room, return an error message that says so.
  *
  * @param {Object[]} dinosaurs - An array of dinosaur objects. See the `data/dinosaurs.js` file for an example of the input.
  * @param {Object[]} rooms - An array of room objects. See the `data/rooms.js` file for an example of the input.
@@ -25,12 +27,30 @@ const exampleRoomData = require("../data/rooms");
  *  getRoomByDinosaurName(dinosaurs, rooms, "Pterodactyl");
  *  //> "Dinosaur with name 'Pterodactyl' cannot be found."
  */
-function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {}
+function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {
+  let roomMessage = `Dinosaur with name '${dinosaurName}' cannot be found.`;
+
+  for (let dinosaur of dinosaurs) {
+    if (dinosaur.name === dinosaurName) {
+      roomMessage = `Dinosaur with name '${dinosaurName}' cannot be found in any rooms.`;
+
+      for (let room of rooms) {
+        if (room.dinosaurs.includes(dinosaur.dinosaurId)) {
+          roomMessage = room.name;
+        }
+      }
+    }
+  }
+  return roomMessage;
+}
+
+console.log(getConnectedRoomNamesById(exampleRoomData, exampleDinosaurData));
 
 /**
  * getConnectedRoomNamesById()
  * ---------------------
- * Returns an array of strings, where each string is the name of a room connected to the given room. If a room ID cannot be found, an error message is returned.
+ * Returns an array of strings, where each string is the name of a room connected to the given room. If a room ID cannot be found, 
+ * an error message is returned.
  *
  * @param {Object[]} rooms - An array of room objects. See the `data/rooms.js` file for an example of the input.
  * @param {string} id - A unique room identifier.
@@ -49,7 +69,39 @@ function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {}
       "Kit Hopkins Education Wing"
     ]
  */
-function getConnectedRoomNamesById(rooms, id) {}
+
+function getConnectedRoomNamesById(rooms, id) {
+  let arrayOfRoomNames = new Array();
+
+  let errorMessage = `Room with ID of 'incorrect-id' could not be found.`;
+
+  let connectId = new Array ();
+
+  for (let room of rooms) {
+    connectId.push(room.roomId);
+  }
+
+  for (let room of rooms) {
+    if (id === "incorrect-id") {
+      return errorMessage;
+    }
+
+    for (let i = 0; i < room.connectsTo.length; i++) {
+      if (!connectId.includes(room.connectsTo[i])) {
+        return errorMessage;
+      }
+
+      if (room.connectsTo[i] === id) {
+        arrayOfRoomNames.push(room.name);
+      }
+    }
+  }
+  return arrayOfRoomNames;
+}
+
+console.log(getConnectedRoomNamesById(exampleRoomData, exampleDinosaurData));
+
+//Do not change anything below this line.
 
 module.exports = {
   getRoomByDinosaurName,
