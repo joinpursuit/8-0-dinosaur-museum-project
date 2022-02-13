1c948f2294fb60d6162bb61fb29b8c71c852877e
bowersjuan <bowersjuan@gmail.com>
2022-02-12 23:49:51 -0500

    Final update for room details functions

--- src/02-room-details.js
+++ src/02-room-details.js
@@ -26,26 +26,25 @@ const exampleRoomData = require("../data/rooms");
  *  //> "Dinosaur with name 'Pterodactyl' cannot be found."
  */
 function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {
-  let dinosaurAvailability = `Dinosaur with name '${dinosaurName}' cannot be found.`;
+  let dinosaurId = "";
 
-  for (let room of rooms) {
-    for (let dinosaur of dinosaurs) {
-      // This nested loop goes through the whole dinosaurs array first, for each room, in order to find the dinosaurName there first
-
-      if (dinosaur.name === dinosaurName) {
-        // If the dinosaur exists in the dinosaurs array then we can get its ID code and use it to search for it in the rooms array
-
-        dinosaurAvailability = `Dinosaur with name '${dinosaurName}' cannot be found in any rooms.`;
-        // Reassigns the default statement before even searching in the rooms array in case it can't be found in any room
-
-        if (room.dinosaurs.includes(dinosaur.dinosaurId)) {
-          return room.name; // Returns room name if dinosaur is found in rooms array too
-        }
-      }
+  for (let dinosaur of dinosaurs) {
+    if (dinosaurName === dinosaur.name) {
+      dinosaurId = dinosaur.dinosaurId;
+      break;
     }
   }
-  return dinosaurAvailability;
-}
+  if (!dinosaurId) {
+    return `Dinosaur with name '${dinosaurName}' cannot be found.`;
+  } // Validates existance of dinosaurName in dinosaurs array
+
+  for (let room of rooms) {
+    if (room.dinosaurs.includes(dinosaurId)) {
+      return room.name;
+    } // Validates existance of dinosaurName in the first room where it is encountered and returns that room name
+  }
+  return `Dinosaur with name '${dinosaurName}' cannot be found in any rooms.`;
+} // If dinosaurName is not found in any room it returns this error message
 
 /**
  * getConnectedRoomNamesById()
@@ -70,35 +69,54 @@ function getRoomByDinosaurName(dinosaurs, rooms, dinosaurName) {
     ]
  */
 function getConnectedRoomNamesById(rooms, id) {
-  let connectedRooms = []; // Default accumulator value and type for final output array of connected rooms
-  let existingRooms = []; // Empty array that will hold all possible rooms
+  let connectedRooms = [];
 
-  for (let room of rooms) {
-    existingRooms.push(room.roomId);
-    // Iterates through all possible rooms and pushes each onto the existingRooms array
-    if (room.roomId === id) {
-      connectedRooms = room.connectsTo;
-      // Iterates through each room and pushes the "rooms.connectsTo" array of the room matching the "id" parameter into the connectedRooms accumulator
-    }
-  }
-
-  if (!existingRooms.includes(id)) {
-    return `Room with ID of '${id}' could not be found.`;
-  }
-  // Returns error message if "id" parameter does not refer to an existing room
-
-  for (let i = 0; i < connectedRooms.length; ++i) {
-    if (!existingRooms.includes(connectedRooms[i])) {
-      return `Room with ID of '${connectedRooms[i]}' could not be found.`;
-    }
-    // Returns error message if any element of the connectedRooms array does not refer to an existing room
+  function roomValidator(rooms, id) {
+    let existingRooms = [];
     for (let room of rooms) {
-      if (connectedRooms[i] === room.roomId) {
-        connectedRooms[i] = room.name;
-        // Changes the connectedRooms array elemnts from room ID's to actual names
+      existingRooms.push(room.roomId);
+    }
+    if (!existingRooms.includes(id)) {
+      return `Room with ID of '${id}' could not be found.`;
+    }
+
+    for (let room of rooms) {
+      if (id === room.roomId) {
+        connectedRooms = room.connectsTo;
+      }
+      for (let connectedRoom of connectedRooms) {
+        if (!existingRooms.includes(connectedRoom)) {
+          return `Room with ID of '${connectedRoom}' could not be found.`;
+        }
       }
     }
   }
+  // Validates whether the id parameter is an existing room of the rooms array
+  // If it exists then validates that its connected rooms also exist within the rooms array
+  const validator = roomValidator(rooms, id);
+  if (validator) {
+    return validator;
+  }
+
+  function createConnectedRoomNamesArray(rooms, id) {
+    for (let room of rooms) {
+      if (id === room.roomId) {
+        connectedRooms = room.connectsTo;
+      }
+    }
+    for (let i = 0; i < connectedRooms.length; ++i) {
+      for (let room of rooms) {
+        if (connectedRooms[i] === room.roomId) {
+          connectedRooms[i] = room.name;
+        }
+      }
+    }
+    return connectedRooms;
+  }
+  // Creates an area of the connected rooms to the specified room
+  // Changes the elements in the cretaed array from codes to names
+  connectedRooms = createConnectedRoomNamesArray(rooms, id);
+
   return connectedRooms;
 }
 
