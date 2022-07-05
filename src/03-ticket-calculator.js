613c8fcb1be33115d82926d54f66a7cf40e842e6
shaquala <shaqualafredericks@pursuit.org>
2022-07-05 12:44:52 -0400

    update changes

--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -159,7 +159,37 @@ if (!ticketInfo.extras){
     purchaseTickets(tickets, purchases);
     //> "Ticket type 'discount' cannot be found."
  */
-function purchaseTickets(ticketData, purchases) {}
+function purchaseTickets(ticketData, purchases) {
+
+  let grandTotal =0;
+  let receipt = 'Thankyou for visiting the Dinosaur Museum!\n----------------------------------------';
+  for (let ticket of purchases) {
+   
+    if(typeof price=== 'number'){
+
+      grandTotal += price;
+      receipt += `\n${capitalizeFirstLetter(ticket.entrantType)} ${ticketData[ticketType].description}: $${(price/100).toFixed(2)}`;
+      if(ticket.extras.length){
+
+        receipt += '(';
+        for(let extra of ticket.extras){
+          receipt+=`${ticketData.extras[extra].description}`;
+
+        }
+        receipt = receipt.split('');
+        receipt.pop();
+        receipt=receipt.join('');
+        receipt += ')';
+      }
+ }else{
+
+  return price;
+ }
+  }
+  receipt += `----------------------------------------\nTotal: $`;
+  receipt += String((grandTotal/100).toFixed(2));
+  return receipt;
+}
 
 // Do not change anything below this line.
 module.exports = {
