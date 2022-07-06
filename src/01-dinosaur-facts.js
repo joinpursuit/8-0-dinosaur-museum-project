1957b2a43b2bfe8acd4573a4a273000a91d3fcc7
Isaac Guerrero <guerreroi@infinitytoll.local>
2022-07-06 11:41:14 -0400

    Add getDinosaursAliveMya function to 01-dinosaur-facts.js

--- src/01-dinosaur-facts.js
+++ src/01-dinosaur-facts.js
@@ -71,7 +71,30 @@ function getDinosaurDescription(dinosaurs, id) {}
  *  getDinosaursAliveMya(dinosaurs, 65, "unknown-key");
  *  //> ["WHQcpcOj0G"]
  */
-function getDinosaursAliveMya(dinosaurs, mya, key) {}
+function getDinosaursAliveMya(dinosaurs, mya, key) {
+  // Loop through the array of dinosaurs, check if mya is below or equal to the dino.mya[0] AND above or equal to the 1 element (btw the time frame)
+  // if dino.mya length === 1 check if mya is equal to dino.mya[0] or dino.mya[0] - 1
+  let dinosaursAlive = [];
+  for (let dino of dinosaurs) {
+    if (dino.mya.length === 1 && (dino.mya[0] === mya || dino.mya[0] - 1 === mya)) {
+      if (key) {
+        dinosaursAlive.push(dino[key]);
+      }
+      else {
+        dinosaursAlive.push(dino.dinosaurId);
+      }
+    }
+    else if (dino.mya[0] >= mya && dino.mya[1] <= mya) {
+      if (key) {
+        dinosaursAlive.push(dino[key]);
+      }
+      else {
+        dinosaursAlive.push(dino.dinosaurId);
+      }
+    }
+  }
+  return dinosaursAlive;
+}
 
 module.exports = {
   getLongestDinosaur,
