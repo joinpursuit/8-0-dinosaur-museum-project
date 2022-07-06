567a2c8dc3d19d9f9e13f445a3c00831b758e3b6
hector918 <hector_zhong@outlook.com>
2022-07-05 23:18:31 -0400

    dinosaur museum project first submit by hector

--- src/01-dinosaur-facts.js
+++ src/01-dinosaur-facts.js
@@ -22,7 +22,38 @@ const exampleDinosaurData = require("../data/dinosaurs");
  *  getLongestDinosaur(dinosaurs);
  *  //> { Brachiosaurus: 98.43 }
  */
-function getLongestDinosaur(dinosaurs) {}
+function getLongestDinosaur(dinosaurs) {
+  // let theLongest ={name:"",height:0};
+  // if(dinosaurs.length===0)
+  // {
+  //   return {}
+  // }
+  // for(let dinosaur of dinosaurs)
+  // {
+  //   if(theLongest["height"]<dinosaur['lengthInMeters'])
+  //   {
+  //     theLongest['name']=dinosaur['name'];
+  //     theLongest['height']=dinosaur['lengthInMeters'];
+  //   }
+  // }
+  // return {[theLongest['name']]:theLongest['height']*3.281};
+
+  let longestDino = {};
+  if (dinosaurs.length === 0) {
+    return {};
+  }
+   let firstDino = dinosaurs[0] ;
+   for (let dinosaur in dinosaurs) {
+    if (dinosaurs[dinosaur].lengthInMeters > firstDino.lengthInMeters) {
+      firstDino = dinosaurs[dinosaur];
+      
+    } 
+    
+    
+   } 
+   var metersToFeet = firstDino.lengthInMeters * 3.281;
+   return {[firstDino.name]:metersToFeet};
+}
 
 /**
  * getDinosaurDescription()
@@ -44,7 +75,20 @@ function getLongestDinosaur(dinosaurs) {}
  *  getDinosaurDescription(dinosaurs, "incorrect-id");
  *  //> "A dinosaur with an ID of 'incorrect-id' cannot be found."
  */
-function getDinosaurDescription(dinosaurs, id) {}
+function getDinosaurDescription(dinosaurs, id) {
+  let result = `A dinosaur with an ID of '${id}' cannot be found.`;
+  for(let x of dinosaurs)
+  {
+    if(x['dinosaurId']===id)
+    {
+      result = `${x.name} (${x['pronunciation']})\n${x.info} It lived in the ${x.period} period, over ${x.mya[x.mya.length-1]} million years ago.`
+    
+      break;
+      
+    }
+  }
+  return result;
+}
 
 /**
  * getDinosaursAliveMya()
@@ -71,7 +115,28 @@ function getDinosaurDescription(dinosaurs, id) {}
  *  getDinosaursAliveMya(dinosaurs, 65, "unknown-key");
  *  //> ["WHQcpcOj0G"]
  */
-function getDinosaursAliveMya(dinosaurs, mya, key) {}
+function getDinosaursAliveMya(dinosaurs, mya, key) {
+  let arrResult = [];
+  let resultIndex =key?key:"dinosaurId";
+  for(let x of dinosaurs)
+  {
+    if((Math.min(...x.mya)<=mya&&Math.max(...x.mya)>=mya))
+    {
+      
+      arrResult.push(x[resultIndex])
+    }
+    else if(x.mya.length===1)
+    {
+      if(x.mya[0]===mya||x.mya[0]-1===mya)
+      {
+        arrResult.push(x[resultIndex])
+      }
+    }
+
+  }
+  
+  return arrResult;
+}
 
 module.exports = {
   getLongestDinosaur,
--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -54,7 +54,45 @@ const exampleTicketData = require("../data/tickets");
     calculateTicketPrice(tickets, ticketInfo);
     //> "Entrant type 'kid' cannot be found."
  */
-function calculateTicketPrice(ticketData, ticketInfo) {}
+function calculateTicketPrice(ticketData, ticketInfo) {
+
+
+  //check ticket type
+  if(ticketData[ticketInfo['ticketType']]===undefined)
+  {
+    return `Ticket type 'incorrect-type' cannot be found.`;
+  }
+
+  //check price
+  let basePrice = ticketData[ticketInfo['ticketType']].priceInCents[ticketInfo.entrantType];
+  if(basePrice===undefined)
+  {
+    return "Entrant type 'incorrect-entrant' cannot be found.";
+  }
+
+  //check extras
+  let extras=[];
+  let extrasPrice=0;
+  if(ticketInfo['extras'].length>0)
+  {
+
+    for(let x of ticketInfo['extras'])
+    {
+
+      if(ticketData['extras'][x]===undefined)
+      {
+        return "Extra type 'incorrect-extra' cannot be found.";
+      }
+      extras.push(ticketData['extras'][x]);
+      
+      extrasPrice+=ticketData['extras'][x].priceInCents[ticketInfo.entrantType];
+    }
+  }
+  // console.log(ticketData,ticketInfo,extras,extrasPrice);
+
+  return basePrice + extrasPrice;
+
+}
 
 /**
  * purchaseTickets()
@@ -109,7 +147,41 @@ function calculateTicketPrice(ticketData, ticketInfo) {}
     purchaseTickets(tickets, purchases);
     //> "Ticket type 'discount' cannot be found."
  */
-function purchaseTickets(ticketData, purchases) {}
+function rcw(text)//return captial work
+{
+  if(typeof text==="string")
+  {
+    return text[0].toUpperCase()+text.slice(1);
+  }
+  return text;
+}
+function purchaseTickets(ticketData, purchases) {
+  let result="";
+  let total=0;
+  for(let x of purchases)
+  {
+    let tmp = calculateTicketPrice(ticketData,x);
+    if(typeof tmp!=="number")
+    {
+      return tmp;
+    }
+    total+=tmp;
+    
+    let subExtras = x['extras'].map((key)=>rcw(key)+ " Access").join(", ")
+    if(subExtras!=="")
+    {
+      subExtras=` (${subExtras})`;
+    }
+    else
+    {
+      subExtras="";
+    }
+
+    result+=`${rcw(x.entrantType)} ${rcw(x.ticketType)} Admission: $${(tmp/100).toFixed(2)}${subExtras}\n`;
+
+  }
+  return `Thank you for visiting the Dinosaur Museum!\n-------------------------------------------\n${result}-------------------------------------------\nTOTAL: $${(total/100).toFixed(2)}`;
+}
 
 // Do not change anything below this line.
 module.exports = {
