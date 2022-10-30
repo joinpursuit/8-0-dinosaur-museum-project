5ede4da11fc4b986da611a6ae8545efac2e877fe
Diegocarrero <diegocar2413@gmail.com>
2022-10-30 16:29:58 -0400

    Added comments to Ticket Calculator file

--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -68,24 +68,36 @@ const exampleTicketData = require("../data/tickets");
     //> "Entrant type 'kid' cannot be found."
  */
 function calculateTicketPrice(ticketData, ticketInfo) {
+  // Creates variable to be updated and returned as the ticket's price in cents
   let costInCents = 0;
+
+  // Returns error if the type of ticket is incorrect
   if (ticketInfo.ticketType != 'general' && ticketInfo.ticketType != 'membership') {
     return `Ticket type '${ticketInfo.ticketType}' cannot be found.`;
   }
+  // Returns error if any of values of extras are incorrect
   for (let a = 0; a < ticketInfo.extras.length; a++) {
     if (ticketInfo.extras[a] != 'movie' && ticketInfo.extras[a] != 'education' && ticketInfo.extras[a] != 'terrace') {
       return "Extra type 'incorrect-extra' cannot be found.";
     }
   }
+  // Returns error if the type of entrant is incorrect
   if (ticketInfo.entrantType != 'child' && ticketInfo.entrantType != 'adult' && ticketInfo.entrantType != 'senior') {
     return "Entrant type 'incorrect-entrant' cannot be found.";
   }
+
+  // If the entrant is a child
   if (ticketInfo.entrantType == 'child') {
+    // If the type of ticket is general
     if (ticketInfo.ticketType == 'general') {
+      // Add corresponding value to the cost of the ticket
       costInCents += 2000;
+    // Else if the type of ticket is membership
     } else {
+      // Add corresponding value to the cost of the ticket
       costInCents += 1500;
     }
+  // The rest of the code in this function is fairly repetitive, straight-forward and self-explanatory
     if (ticketInfo.extras.includes('movie')) {
       costInCents += 1000;
     }
@@ -126,6 +138,7 @@ function calculateTicketPrice(ticketData, ticketInfo) {
       costInCents += 1000;
     }
   }
+  // Returns the total cost of the ticket in cents, taking into account the type of ticket, type of entrant and all the extras
   return costInCents;
 }
 
@@ -190,23 +203,29 @@ function calculateTicketPrice(ticketData, ticketInfo) {
     //> "Ticket type 'discount' cannot be found."
  */
 function purchaseTickets(ticketData, purchases) {
-  
+  // Creates string variable of 'receipt' that will be modified accordingly and returned
   let receipt = 'Thank you for visiting the Dinosaur Museum!\n-------------------------------------------\n';
+  // Creates number variable of 'priceTotal' that will be updated and accessed accordingly
   let priceTotal = 0;
+
+  // Iterates through the 'purchases' array of objects
   for (let a = 0; a < purchases.length; a++) {
+    // Returns error if the type of ticket is incorrect
     if (purchases[a].ticketType != 'general' && purchases[a].ticketType != 'membership') {
       return `Ticket type '${purchases[a].ticketType}' cannot be found.`;
     }
+    // Returns error if any of the extras are incorrect
     for (let b = 0; b < purchases[a].extras.length; b++) {
       if (purchases[a].extras[b] != 'movie' && purchases[a].extras[b] != 'education' && purchases[a].extras[b] != 'terrace') {
         return "Extra type 'incorrect-extra' cannot be found.";
       }
     }
+    // Returns error if the type of entrant is incorrect
     if (purchases[a].entrantType != 'child' && purchases[a].entrantType != 'adult' && purchases[a].entrantType != 'senior') {
       return "Entrant type 'incorrect-entrant' cannot be found.";
     }
 
-    //> "Thank you for visiting the Dinosaur Museum!\n-------------------------------------------\nAdult General Admission: $50.00 (Movie Access, Terrace Access)\nSenior General Admission: $35.00 (Terrace Access)\nChild General Admission: $45.00 (Education Access, Movie Access, Terrace Access)\nChild General Admission: $45.00 (Education Access, Movie Access, Terrace Access)\n-------------------------------------------\nTOTAL: $175.00"
+    // Adds the corresponding type of entrant to the receipt with correct formatting
     if (purchases[a].entrantType == 'child') {
       receipt += 'Child ';
     } else if (purchases[a].entrantType == 'adult') {
@@ -215,18 +234,23 @@ function purchaseTickets(ticketData, purchases) {
       receipt += 'Senior ';
     }
 
+    // Adds the corresponding type of ticket to the receipt with correct formatting
     if (purchases[a].ticketType == 'general') {
       receipt += 'General ';
     } else {
       receipt += 'Membership ';
     }
 
+    // Sets the price in dollars of each purchase to a new variable 'priceOfEachPurchase'
     let priceOfEachPurchase = calculateTicketPrice(ticketData, purchases[a]) / 100;
+    // Updates the 'priceTotal' to add current purchase price to all previous purchases 
     priceTotal += priceOfEachPurchase;
+    // Adds corresponding purchase cost with correct formatting
     receipt += `Admission: $${priceOfEachPurchase.toFixed(2)}`;
     
-    
+    // Creates empty array that will be filled with corresponding extras
     let extrasArray = [];
+    // 254-262 Adds corresponding extras to array of extras
     if (purchases[a].extras.includes('education')) {
       extrasArray.push('Education Access');
     }
@@ -236,17 +260,25 @@ function purchaseTickets(ticketData, purchases) {
     if (purchases[a].extras.includes('terrace')) {
       extrasArray.push('Terrace Access');
     }
+    // Swaps indexes of 'education' and 'terrace' if 'movie' is not present
     if (purchases[a].extras.includes('terrace') && purchases[a].extras.includes('education') && !purchases[a].extras.includes('movie')) {
       extrasArray[0] = extrasArray.splice(1, 1, extrasArray[0]);
     }
+    
+    // If the array of extras is not empty
     if (extrasArray.length != 0) {
+      // Creates a string of all the elements in the array of extras, joined by a comma and a space if more than one
       const extrasString = extrasArray.join(', ');
+      // Adds the string of extras to the receipt with correct formatting
       receipt += ` (${extrasString})\n`;    
     } else {
+      // If the array of extras is empty, just adds a new line to receipt
       receipt += '\n'
     }
   }
+  // Adds the total price of all purchases with correct formatting
   receipt += `-------------------------------------------\nTOTAL: $${priceTotal.toFixed(2)}`;
+  // Returns final receipt
   return receipt;
 }
 
