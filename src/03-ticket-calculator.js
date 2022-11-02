9c94d9c9ac90608d0b64247bd02e9fa28dd12856
Wilsantos1975 <Faustosantos@pursuit.org>
2022-11-01 21:20:46 -0400

    work on the last problem

--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -59,12 +59,15 @@ function calculateTicketPrice(ticketData, ticketInfo) {
   let cost = 0;
   if (!ticketData[ticketInfo.ticketType]) {
     return "Ticket type 'incorrect-type' cannot be found.";
-  } else if (!ticketData["general"].priceInCents[ticketInfo.entrantType]) {
+  } else if (
+    !ticketData[ticketInfo.ticketType].priceInCents[ticketInfo.entrantType]
+  ) {
     return `Entrant type 'incorrect-entrant' cannot be found.`;
   } else {
     cost =
       ticketData[ticketInfo.ticketType].priceInCents[ticketInfo.entrantType];
   }
+
   for (let i = 0; i < ticketInfo.extras.length; i++) {
     switch (ticketInfo.extras[i]) {
       case "movie":
@@ -138,8 +141,38 @@ function calculateTicketPrice(ticketData, ticketInfo) {
     purchaseTickets(tickets, purchases);
     //> "Ticket type 'discount' cannot be found."
  */
-function purchaseTickets(ticketData, purchases) {}
+function purchaseTickets(ticketData, purchases) {
+  // let receipt = "";
+  let receiptData = "";
+  let finalPrice = 0;
+  let total = 0;
 
+  for (let i = 0; i < purchases.length; i++) {
+    if (purchases[i].extras.includes("incorrect-extra"))
+      return "Extra type 'incorrect-extra' cannot be found.";
+
+    if (!ticketData[purchases[i].ticketType]) {
+      return `Ticket type '${purchases[i].ticketType}' cannot be found.`;
+    } else if (
+      !ticketData[purchases[i].ticketType].priceInCents[
+        purchases[i].entrantType
+      ]
+    ) {
+      return "Entrant type 'incorrect-entrant' cannot be found.";
+    } else {
+      finalPrice += calculateTicketPrice(ticketData, purchases[i]);
+      receiptData = `Thank you for visiting the Dinosaur Museum!\n-------------------------------------------\n${
+        purchases[i].entrantType
+      } ${purchases[i].ticketType} Admission: $${
+        finalPrice / 100
+      }.00 \n----------------------------
+      ---------------\nTOTAL: $${total}`;
+    }
+    total += finalPrice;
+  }
+
+  return receiptData;
+}
 // Do not change anything below this line.
 module.exports = {
   calculateTicketPrice,
